c70e813ccf5dc4211c8e7594049b012dff434c2a
hstravis <hstravis@users.noreply.github.com>
2014-10-28 16:32:19 -0600

    Override default ModelDefinition values
    I have my own attributes that extend OrmLite but there is no way to override the behavior of ORM Lite. Added an optional callback that is invoked after the ModelDefinition is created.

--- src/ServiceStack.OrmLite/OrmLiteConfigExtensions.cs
+++ src/ServiceStack.OrmLite/OrmLiteConfigExtensions.cs
@@ -43,7 +43,9 @@ namespace ServiceStack.OrmLite
         {
             typeModelDefinitionMap = new Dictionary<Type, ModelDefinition>();
         }
-
+        
+        public static Action<ModelDefinition> InitModelDefinitionCallback { get; set; }
+        
         internal static ModelDefinition GetModelDefinition(this Type modelType)
         {
             ModelDefinition modelDef;
@@ -202,6 +204,9 @@ namespace ServiceStack.OrmLite
                 Interlocked.CompareExchange(ref typeModelDefinitionMap, newCache, snapshot), snapshot));
 
             LicenseUtils.AssertValidUsage(LicenseFeature.OrmLite, QuotaType.Tables, typeModelDefinitionMap.Count);
+            
+            if (InitModelDefinitionCallback != null)
+                InitModelDefinitionCallback(modelDef);
 
             return modelDef;
         }
